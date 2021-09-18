cfda0b17c3673ba93ac67ad7d82b3759d64fe4bb
KRyan23 <GitHub@kryan23.bulc.club>
2021-09-18 14:11:24 +0000

    Spreadsheet in Google docs not shared correctly.Fixed

---
+++ run.py
@@ -0,0 +1,22 @@
+#To install gspread and auth enter the command below in the terminal
+#pip3 install gspread google-auth
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
+#To run this type the following command python3 run.py
